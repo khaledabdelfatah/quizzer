21a7b6ea29275b99593e4b4e22ac56f86087be5a
KerolosKmal <48252882+KerolosKmal@users.noreply.github.com>
2019-12-08 17:05:55 +0200

    edit

--- js/createquiz.js
+++ js/createquiz.js
@@ -1,5 +1,8 @@
+//var firebase = require("../app.js");
+
 let num_Of_questions = 0;
- function f(s) {
+
+function f(s) {
     var x = document.getElementById(s);
     var z = x.getElementsByTagName('textarea');
     var y = x.getElementsByTagName('input');
@@ -102,3 +105,51 @@ function add_one_question(){
     child.innerHTML = str;
     document.getElementById('questions').appendChild(child);
 }
+
+
+//var firebaseConfig = {
+//    apiKey: "AIzaSyD0tMq6ZWGaJcG4VFWcmETbTxO7IOdDE3Q",
+//    authDomain: "quizzer-a0c6e.firebaseapp.com",
+//    databaseURL: "https://quizzer-a0c6e.firebaseio.com",
+//    projectId: "quizzer-a0c6e",
+//    storageBucket: "quizzer-a0c6e.appspot.com",
+//    messagingSenderId: "235551414761",
+//    appId: "1:235551414761:web:cb2e8ef047394bf666d378",
+//    measurementId: "G-GBG3VNHGE6"
+//};
+//// Initialize Firebase
+//firebase.initializeApp(firebaseConfig);
+
+
+function on(){
+    
+var firestore = firebase.firestore();
+const docRef = firestore.collection("KI").doc("NA");
+
+
+//const testbutton = document.getElementById("finish");
+//testbutton.addEventListener("click", function () {
+    console.log("Im here Finally***************************************");
+    docRef.set({
+        name: "Kiro"
+    });
+
+}
+//});
+
+//   
+
+
+
+//var loadbu=document.getElementById("getbutoon");
+//loadbu.addEventListener("click",function(){
+//docRef.get().then(function(doc){
+// if(doc && doc.exists){
+//     const mydata=doc.data();
+//     console.log(mydata.name);
+//     
+// }   
+//}
+//    )
+//
+//});
