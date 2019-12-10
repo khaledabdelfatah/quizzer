545837f45df7a45d8169e1331bb3d43257faacd2
eslam5elesawy <33846533+eslam5elesawy@users.noreply.github.com>
2019-12-10 21:39:07 +0200

    update login form in home page and add firebase

--- app.js
+++ app.js
@@ -11,32 +11,75 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+//
+//
 
-//var firestore = firebase.firestore();
-//const docRef = firestore.collection("Hello").doc("namei");
-////const testbutton = document.getElementById("getbutoon");
-////testbutton.addEventListener("click", function () {
-////    console.log("Im here Finally***************************************");
-////    docRef.set({
-////        name: "yarab"
-////    });
-////
-////
-////});
-//
-////   
-//
-//
-//
-//var loadbu=document.getElementById("getbutoon");
-//loadbu.addEventListener("click",function(){
-//docRef.get().then(function(doc){
-// if(doc && doc.exists){
-//     const mydata=doc.data();
-//     console.log(mydata.name);
-//     
-// }   
-//}
-//    )
-//
-//});
+var firestore = firebase.firestore();
+const docRef = firestore.collection("try").doc("name1");
+const testbutton = document.getElementById("getbutoon");
+testbutton.addEventListener("click", function () {
+
+    firebase.auth().createUserWithEmailAndPassword("eslam@gmail.com", "jhgjhgjhgj").catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorMessage);
+        // ...
+    });
+
+});
+
+const autho = firebase.auth();
+
+autho.onAuthStateChanged(function (user) {
+
+    if (user) {
+
+        firestore.collection("User").doc(autho.getUid()).get().then(function (doc) {
+            if (doc.exists) {
+
+                if (doc.data().remmber == true) {
+                    //  window.location.href = "html-page/profile.html";
+                } else {
+                    autho.signOut();
+                }
+
+            }
+        })
+    }
+});
+
+autho.signOut();
+
+function login() {
+
+    const email = document.getElementById("Email").value;
+    const password = document.getElementById("Password").value;
+    // const Remmber = document.getElementById("Remmber").checked;
+
+    autho.signInWithEmailAndPassword(email, password).catch(function (error) {
+
+        document.getElementById("errMes").innerHTML = error.message;
+
+        //        if (Remmber) {
+        //            firestore.collection("User").doc(autho.getUid()).add({
+        //                remmber: true
+        //            });
+        //        } else {
+        //            firestore.collection("User").doc(autho.getUid()).add({
+        //                remmber: false
+        //            });
+        //
+        //        }
+    });
+
+    autho.onAuthStateChanged(function (user) {
+        if (user) {
+
+            window.location.href = "html-page/profile.html";
+
+        } else {
+
+        }
+    });
+}
