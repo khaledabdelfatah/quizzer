1d948f82c768a490a364b75dad1cda71454c806e
eslam5elesawy <33846533+eslam5elesawy@users.noreply.github.com>
2019-12-18 18:34:55 +0200

    update profile.js for user avatar

--- js/profile.js
+++ js/profile.js
@@ -1,22 +1,15 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyD0tMq6ZWGaJcG4VFWcmETbTxO7IOdDE3Q",
-    authDomain: "quizzer-a0c6e.firebaseapp.com",
-    databaseURL: "https://quizzer-a0c6e.firebaseio.com",
-    projectId: "quizzer-a0c6e",
-    storageBucket: "quizzer-a0c6e.appspot.com",
-    messagingSenderId: "235551414761",
-    appId: "1:235551414761:web:cb2e8ef047394bf666d378",
-    measurementId: "G-GBG3VNHGE6"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 var autho = firebase.auth();
 var firestore = firebase.firestore();
 
 
 autho.onAuthStateChanged(function (user) {
     if (user) {
+        
+        document.getElementById("nav-avatar").style.display = "block";
+            firebase.firestore().collection("User").doc(firebase.auth().getUid()).get().then(function (doc) {
+                document.getElementById("profile-name-avatar").innerHTML = doc.data().firstname +" " + doc.data().lastName;
+            });
+        
         firestore.collection("User").doc(autho.getUid()).get().then(function (doc) {
             if (doc.exists) {
                 const mydata = doc.data();
